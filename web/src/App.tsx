6374a023edb4ca62968347641167e50387f7ed73
Lucas Prazeres <lucascprazeres@outlook.com>
2023-01-16 21:31:34 -0300

    feat: setup web app

---
+++ web/src/App.tsx
@@ -0,0 +1,14 @@
+import './styles/global.css'
+
+import { Habit } from "./components/Habit";
+
+export function App() {
+  return (
+   <>
+    <Habit completed={3}/>
+    <Habit completed={3}/>
+    <Habit completed={3}/>
+    <Habit completed={3}/>
+   </>
+  )
+}
