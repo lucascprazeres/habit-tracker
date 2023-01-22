02494fb698bf0bd2f1e144c3ebacab4173663d7a
Lucas Prazeres <lucascprazeres@outlook.com>
2023-01-22 16:05:22 -0300

    feat(web): integrates summary and habit creation to the backend

--- web/src/components/SummaryTable.tsx
+++ web/src/components/SummaryTable.tsx
@@ -1,4 +1,6 @@
-import { useId } from "react"
+import { useEffect, useId, useState } from "react"
+import dayjs from 'dayjs'
+import { api } from "../lib/axios"
 import { generateDatesFromYearBeginning } from "../utils/generate-dates-from-year-beginning"
 import { HabitDay } from "./HabitDay"
 
@@ -8,7 +10,21 @@ const summaryDates = generateDatesFromYearBeginning()
 const minimumSummaryDates = 18 * 7 // 18 weeks
 const amountOfDaysToFill = minimumSummaryDates - summaryDates.length
 
+type Summary = Array<{
+  id: string
+  date: string
+  amount: number
+  completed: number
+}>
+
 export function SummaryTable() {
+  const [summary, setSummary] = useState<Summary>([])
+
+  useEffect(() => {
+    api.get('/summary')
+      .then(response => setSummary(response.data))
+  }, [])
+
   return (
     <div className="w-full flex">
       <div className="grid grid-rows-7 grid-flow-row">
@@ -20,16 +36,23 @@ export function SummaryTable() {
       </div>
 
       <div className="grid grid-rows-7 grid-flow-col gap-3">
-        {summaryDates.map(() => (
-          <HabitDay
-            key={useId()}
-            amount={5}
-            completed={Math.round(Math.random() * 5)}
-          />
-        ))}
+        {summaryDates.map((date) => {
+          const dayInSummary = summary.find(day => {
+            return dayjs(date).isSame(day.date, 'day')
+          })
+          
+          return (
+            <HabitDay
+              key={useId()}
+              date={date}
+              amount={dayInSummary?.amount}
+              completed={dayInSummary?.completed}
+            />
+          )
+        })}
 
         {amountOfDaysToFill > 0 && Array.from({ length: amountOfDaysToFill }).map(() => (
-          <HabitDay key={useId()} disabled />
+          <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed"/>
         ))}
       </div>
     </div>
